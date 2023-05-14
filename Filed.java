518d99fcb68e2dd42fcaf8e3d2c70166a335e7a6
yyyuuu1 <3054029490@qq.com>
2023-05-15 00:04:18 +0800

    属性的基本使用

---
+++ Filed.java
@@ -0,0 +1,21 @@
+package com.objectOriented;
+
+/**
+ * 讲解属性和成员变量
+ */
+public class Filed {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+class T1{
+
+    //1.从概念或叫法上看成员变量=属性=（field）字段   （即  成员变量是用来表示属性的，成员变量也叫做属性）
+    String name;//这个就叫做属性
+    public int age;//还可以带上访问修饰符
+
+    //2.属性是类的一个组成部分，一般是基本数据类型，也可以是引用数据类型（对象，数组）
+    int arr[];//还可以是一个数组
+}
