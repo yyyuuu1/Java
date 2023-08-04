4767bceb7d5f76a14d0ef283e25f742d91e65466
yyyuuu1 <3054029490@qq.com>
2023-08-04 14:53:33 +0800

    继承的细节、

---
+++ extendsDetail.java
@@ -0,0 +1,14 @@
+package idea.chapter08.extends_;
+
+/**
+ * 演示继承的注意事项和使用细节
+ */
+public class extendsDetail {
+    public static void main(String[] args) {
+        //student student = new student();
+    }
+}
+
+
+
+
